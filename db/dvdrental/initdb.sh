5a942541c3d506c3d2ec563651089043ef786da1
Leon Rogers <Leon.Rogers@BlueElephantDBA.co.uk>
2024-06-26 17:54:07 +0100

    Split databases into individual containers.

--- db/dvdrental/initdb.sh
+++ db/dvdrental/initdb.sh
@@ -17,10 +17,6 @@ psql -v ON_ERROR_STP=1 --username "dbo" --dbname "postgres" <<-ESQL
     CREATE DATABASE dvdrental TEMPLATE template1;
     COMMENT ON DATABASE dvdrental IS 'Home of dvdrental database. this attempts to use all manner of DDL, DML.';
     ALTER DATABASE dvdrental SET search_path TO rental,sqitch,pgtap;
-
-    CREATE DATABASE pgtapme TEMPLATE template1;
-    COMMENT ON DATABASE pgtapme IS 'Home of pgtapme test database. this attempts to use all manner of DDL, DML.';
-    ALTER DATABASE pgtapme SET search_path TO pgtapme,exts,sqitch,pgtap;
 ESQL
 
 psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-ESQL
