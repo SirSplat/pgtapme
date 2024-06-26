234f2edfa2ce1fa6fa19c472d716298b18967ace
Leon Rogers <Leon.Rogers@BlueElephantDBA.co.uk>
2024-06-26 14:32:34 +0100

    Add dvdrental alternative design

--- db/initdb.sh
+++ db/initdb.sh
@@ -22,6 +22,10 @@ psql -v ON_ERROR_STP=1 --username "dbo" --dbname "postgres" <<-ESQL
     COMMENT ON DATABASE dvdrental IS 'Home of dvdrental database. this attempts to use all manner of DDL, DML.';
     ALTER DATABASE dvdrental SET search_path TO rental,sqitch,pgtap,partman;
 
+    CREATE DATABASE dvdrental_alt TEMPLATE template1;
+    COMMENT ON DATABASE dvdrental_alt IS 'Home of dvdrental_alt database. This attempts to use design and performance best pratices.';
+    ALTER DATABASE dvdrental_alt SET search_path TO rental,sqitch,pgtap,partman;
+
     CREATE DATABASE pgtapme TEMPLATE template1;
     COMMENT ON DATABASE pgtapme IS 'Home of pgtapme test database. this attempts to use all manner of DDL, DML.';
     ALTER DATABASE pgtapme SET search_path TO pgtapme,pgtapme_ext,sqitch,pgtap,partman;
