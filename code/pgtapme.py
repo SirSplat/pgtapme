f80b61b6fe40322166704bcd869392caf6c87201
Leon Rogers <Leon.Rogers@BlueElephantDBA.co.uk>
2024-07-03 15:42:03 +0100

    Added consolidated logging and improved error handling while maintaining clarity and readability.

--- code/pgtapme.py
+++ code/pgtapme.py
@@ -4,12 +4,13 @@ import os
 from importlib import import_module
 
 from psycopg2 import extras
-
 from src.helpers import configure_logging, connect_to_database, get_modules
 
 
 def main() -> None:
     configure_logging()
+
+    logging.debug("Script started.")
 
     script_directory = os.path.dirname(os.path.realpath(__file__))
     config_path = os.path.join(script_directory, "config.json")
@@ -17,37 +18,53 @@ def main() -> None:
     try:
         with open(config_path, "r") as config_file:
             config = json.load(config_file)
-    except FileNotFoundError as e:
-        logging.error(f"Error: config.json file not found. {e}")
+        logging.debug("Configuration loaded succesfully.")
+    except FileNotFoundError:
+        logging.error("config.json file not found.")
         return
     except json.JSONDecodeError as e:
-        logging.error(f"Error: decoding config.json. {e}")
+        logging.error(f"Error decoding config.json: {e}")
         return
     except KeyError as e:
-        logging.error(f"Error: missing or unfound config key. {e}")
+        logging.error(f"Missing or invalid config key: {e}")
+        return
     except Exception as e:
-        logging.error(f"An unexpected error occurred: {e}", exc_info=True)
+        logging.error(
+            "An unexpected error occurred while loading the config.", exc_info=True
+        )
+        return
 
     conn, dbname = connect_to_database()
+
+    logging.debug(f"Connected to database: {dbname}")
+
     cursor = conn.cursor(cursor_factory=extras.NamedTupleCursor)
     output_dir = os.path.join(config["output_dir"], dbname)
 
-    logging.debug("Reverse engineer schema started.")
+    logging.debug(f"Output directory set to: {output_dir}")
+    logging.debug(f"Starting reverse engineering of database: {dbname}")
 
     module_types = get_modules(config)
 
     for module_type in module_types:
+        logging.debug(f"Processing module: {module_type}")
+
         module = import_module(f"src.module_types.{module_type}", "t")
         process_data_function = getattr(module, "process_data", None)
 
         if process_data_function:
             process_data_function(cursor, output_dir, module_type)
+            logging.debug(f"Completed processing module type: {module_type}")
+        else:
+            logging.debug(f"No process_data function found for module type: {module_type}")
 
-    logging.debug("Reverse engineer schema completed.")
+    logging.debug("Completed reverse engineering of schema.")
 
     cursor.close()
     conn.close()
 
+    logging.debug("Database connection closed.")
+    logging.debug("Script finished.")
 
 if __name__ == "__main__":
     main()
--- code/src/helpers.py
+++ code/src/helpers.py
@@ -9,6 +9,8 @@ from dotenv import load_dotenv
 
 
 def parse_command_line_args():
+    logging.debug("Parsing command-lins arguments.")
+
     """
     Parse command-line arguments for logging and module type configuration.
 
@@ -45,10 +47,16 @@ def parse_command_line_args():
     parser.add_argument(
         "--db-port", dest="database_port", default=None, help="Override database port"
     )
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    logging.debug(f"Command-line arguments parsed: {args}")
+
+    return args
 
 
 def override_module_type(config, module_type):
+    logging.debug(f"Overriding module types with: {module_type}")
+
     """
     Override the 'module_types' in the given configuration if a test type is provided.
 
@@ -58,9 +66,14 @@ def override_module_type(config, module_type):
     """
     if module_type:
         config["module_types"] = [module_type]
+        logging.debug(f"Module types overridden: {config['module_types']}")
+    else:
+        logging.debug("No module type overide provided.")
 
 
 def get_modules(config):
+    logging.debug("Retrieving module types from configuration.")
+
     """
     Get the module types from command-line arguments and configuration.
 
@@ -73,18 +86,28 @@ def get_modules(config):
     args = parse_command_line_args()
     configure_logging_from_args(args)
     override_module_type(config, args.module_type)
-    return config.get("module_types", [])
+    modules = config.get("module_types", [])
+
+    logging.debug(f"Modules to process: {modules}")
+
+    return modules
 
 
 def configure_logging():
+    logging.debug("Configuring logging.")
+
     args = parse_command_line_args()
 
     # Configure logging based on the command line argument if provided.
     # If --log-level argument is not provided, keep the default level as WARNING.
     configure_logging_from_args(args)
 
+    logging.debug(f"Logging configured with level: {args.log_level}")
+
 
 def configure_logging_from_args(args):
+    logging.debug("Configuring logging from command-line arguments.")
+
     """
     Configure logging based on the command line argument if provided.
     If --log-level argument is not provided, keep the default level as WARNING.
@@ -96,6 +119,8 @@ def configure_logging_from_args(args):
     if hasattr(args, "log_level") and args.log_level:
         # Set the logging level for the root logger
         root_logger.setLevel(getattr(logging, args.log_level))
+
+        logging.debug(f"Logging level set to: {args.log_level}")
     else:
         # Use the default logging configuration
         logging.basicConfig(
@@ -104,8 +129,12 @@ def configure_logging_from_args(args):
             datefmt="%Y-%m-%d %H:%M:%S",
         )
 
+        logging.debug("Default logging configuration applied with level: WARNING")
+
 
 def log_function_call(func=None, log_level=logging.DEBUG, info_message=None):
+    logging.debug("Initializing log_function_call decorator.")
+
     """
     A versatile decorator that logs function calls, including parameter names and values,
     at a specified log level (default: logging.DEBUG). It also logs additional
@@ -158,6 +187,8 @@ def log_function_call(func=None, log_level=logging.DEBUG, info_message=None):
     """
 
     def decorator(func):
+        logging.debug(f"Decorating function: {func.__name__}")
+
         @wraps(func)
         def wrapper(*args, **kwargs):
             func_name = func.__name__
@@ -182,7 +213,12 @@ def log_function_call(func=None, log_level=logging.DEBUG, info_message=None):
             if info_message:
                 logging.info(f"{info_message} {func.__module__}.{func_name}")
 
-            return func(*args, **kwargs)
+            # Execute the function and log the return value
+            result = func(*args, **kwargs)
+
+            logging.log(log_level, f"{func.__module__}.{func_name} returned: {result}")
+
+            return result
 
         return wrapper
 
@@ -196,35 +232,82 @@ def log_function_call(func=None, log_level=logging.DEBUG, info_message=None):
 @log_function_call
 def create_file_path(output_dir, *parts):
     odir = os.path.join(output_dir, *parts[:-1])
+
     if not os.path.exists(odir):
         os.makedirs(odir)
-    return os.path.join(odir, f"{parts[-1]}.sql")
+
+        logging.debug(f"Created directory: {odir}")
+
+    try:
+        file_path = os.path.join(odir, f"{parts[-1]}.sql")
+
+        logging.debug(f"Created file path: {file_path}")
+
+        return file_path
+    except Exception as e:
+        logging.error(f"Failed to create file path: {e}")
+
+        raise
 
 
 @log_function_call
 def replace_test_count(file_path, test_count):
-    with open(file_path, "r") as f:
-        content = f.read()
-    content = content.replace(f"(0)", f"({test_count})")
-    with open(file_path, "w") as f:
-        f.write(content)
+    try:
+        logging.debug(f"Reading file: {file_path}")
+
+        with open(file_path, "r") as f:
+            content = f.read()
+
+        logging.debug(f"Replacing test count in file: {file_path}")
+
+        content = content.replace(f"(0)", f"({test_count})")
+
+        logging.debug(f"Writing updated content to file: {file_path}")
+
+        with open(file_path, "w") as f:
+            f.write(content)
+
+        logging.debug(f"Successfully replaced test count in file: {file_path}")
+    except Exception as e:
+        logging.error(f"Failed to replace test count in file {file_path}: {e}")
+
+        raise
 
 
 @log_function_call
 def count_tests_in_file(file_path):
     word = "SELECT"
     count = 0
-    with open(file_path, "r") as f:
-        for line in f:
-            count += line.split().count(word)
-    # Subtracting 2 to exclude the "SELECT plan(?);" and "SELECT * FROM finish();"
-    return count - 2
+    try:
+        logging.debug(f"Counting occurrences of '{word}' in file: {file_path}")
+
+        with open(file_path, "r") as f:
+            for line in f:
+                count += line.split().count(word)
+
+        # Subtracting 2 to exclude the "SELECT plan(?);" and "SELECT * FROM finish();"
+        test_count = count - 2
+
+        logging.debug(
+            f"Total test count in file (excluding plan and finish): {test_count}"
+        )
+        return test_count
+    except Exception as e:
+        logging.error(f"Failed to count tests in file {file_path}: {e}")
+        raise
 
 
 @log_function_call
 def set_plan_count(test_file_path):
-    test_count = count_tests_in_file(test_file_path)
-    replace_test_count(test_file_path, test_count)
+    try:
+        logging.debug(f"Setting plan count for test file: {test_file_path}")
+        test_count = count_tests_in_file(test_file_path)
+        logging.debug(f"Test count determined: {test_count}")
+        replace_test_count(test_file_path, test_count)
+        logging.debug(f"Plan count set successfully for test file: {test_file_path}")
+    except Exception as e:
+        logging.error(f"Failed to set plan count for test file {test_file_path}: {e}")
+        raise
 
 
 @log_function_call
@@ -251,20 +334,35 @@ def connect_to_database():
             database_port,
         ]
     ):
+        logging.error("Missing one or more required database connection parameters.")
         raise ValueError("Missing one or more required database connection parameters.")
 
-    conn = psycopg2.connect(
-        database=database_name,
-        user=database_user,
-        password=database_user_password,
-        host=database_host,
-        port=database_port,
-    )
-    return conn, database_name
+    try:
+        conn = psycopg2.connect(
+            database=database_name,
+            user=database_user,
+            password=database_user_password,
+            host=database_host,
+            port=database_port,
+        )
+        logging.debug(
+            f"Connected to database: {database_name} at {database_host}:{database_port}"
+        )
+        return conn, database_name
+    except psycopg2.DatabaseError as e:
+        logging.error(f"Database connection failed: {e}")
+        raise
+    except Exception as e:
+        logging.error(
+            f"An unexpected error occurred while connecting to the database: {e}",
+            exc_info=True,
+        )
+        raise
 
 
 @log_function_call()
 def format_array_parameter(parameter):
+    logging.debug(f"Formatting parameter: {parameter}")
     """
     Formats a list of parameters into a PostgreSQL array string.
 
@@ -282,55 +380,87 @@ def format_array_parameter(parameter):
         # Outputs: "ARRAY[]::TEXT[]"
     """
     if parameter is None:
-        return "ARRAY[]::TEXT[]"
+        result = "ARRAY[]::TEXT[]"
     elif isinstance(parameter, (list, tuple)):
-        return f"ARRAY{parameter}::TEXT[]"
+        result = f"ARRAY{parameter}::TEXT[]"
     elif isinstance(parameter, (str)):
-        return f"ARRAY['{parameter}']::TEXT[]"
+        result = f"ARRAY['{parameter}']::TEXT[]"
     else:
+        logging.error("Parameter must be a list, tuple, or string")
         raise TypeError("Parameter must be a list or tuple")
+
+    logging.debug(f"Formatted parameter: {result}")
+    return result
 
 
 @log_function_call
 def format_directory_element(parameter):
+    logging.debug(f"Formatting directory element: {parameter}")
     """
     Removes double quotes and replaces spaces with underscores because I do not
-        like then in a directory and file names
+        like them in directory and file names
+
+    Args:
+        parameter (str): The parameter to be formatted.
+
+    Returns:
+        str: The formatted directory element.
+
+    Raises:
+        TypeError: If the parameter is not a string.
+
+    Examples:
+        >>> format_directory_element('"my file"')
+        'my_file'
     """
-    if isinstance(parameter, (str)):
-        return parameter.strip('"').replace(" ", "_")
+    if isinstance(parameter, str):
+        result = parameter.strip('"').replace(" ", "_")
     else:
+        logging.error("Parameter must be a string")
         raise TypeError("Parameter must be a string")
+
+    logging.debug(f"Formatted directory element: {result}")
+    return result
 
 
 @log_function_call
 def format_single_quote(parameter):
+    logging.debug(f"Formatting single quote for parameter: {parameter}")
     """
     Have been forced to hard code for certain column defaults! not ideal, I know!
 
     Maybe someday I can find a much better solution than this.
     """
-    if "'" in parameter:
-        if "nextval" in parameter or "now" in parameter:
-            result = f"$${parameter}$$"
+    if isinstance(parameter, str):
+        if "'" in parameter:
+            if "nextval" in parameter or "now" in parameter:
+                result = f"$${parameter}$$"
+            else:
+                result = parameter
         else:
-            result = parameter
+            result = f"'{parameter}'"
+        logging.debug(f"Formatted single quote parameter: {result}")
+        return result
     else:
-        result = f"'{parameter}'"
-
-    return result
+        logging.error("Parameter must be a string")
+        raise TypeError("Parameter must be a string")
 
 
 @log_function_call
 def remove_schema(parameter):
+    logging.debug(f"Removing schema from parameter: {parameter}")
     if "." in parameter and parameter is not None:
         split_parts = parameter.split(".")
-        return split_parts[-1]
+        result = split_parts[-1]
+        logging.debug(f"Schema removed, result: {result}")
+        return result
+    logging.debug(f"No schema to remove, result: {parameter}")
     return parameter
 
 
 @log_function_call
 def validate_kinds(reltype, kinds=None):
+    logging.debug(f"Validating kinds for reltype: {reltype}, kinds: {kinds}")
     """
     Validates a list of relation kinds against a predefined set of permissible values for a specific relation type.
 
@@ -383,17 +513,23 @@ def validate_kinds(reltype, kinds=None):
             "e": "enum",
             "p": "pseudo",
             "r": "range",
-            "m": "multirange",
+            "M": "multirange",
         },
     }
 
     valid_kinds = valid_mappings.get(reltype)
     if valid_kinds is None:
+        logging.error(
+            f"Invalid reltype: {reltype}. Valid reltypes are: {list(valid_mappings.keys())}"
+        )
         raise ValueError(
             f"Invalid reltype: {reltype}. Valid reltypes are: {list(valid_mappings.keys())}"
         )
 
     if not kinds:
+        logging.error(
+            f"Invalid kinds must not be empty for reltype '{reltype}'. Valid kinds are: {list(valid_kinds.keys())}"
+        )
         raise ValueError(
             f"Invalid kinds must not be empty for reltype '{reltype}'. Valid kinds are: {list(valid_kinds.keys())}"
         )
@@ -401,6 +537,11 @@ def validate_kinds(reltype, kinds=None):
     invalid_kinds = [k for k in kinds if k not in valid_kinds]
     if invalid_kinds:
         invalid_str = ", ".join(invalid_kinds)
+        logging.error(
+            f"Invalid kind value(s) for reltype '{reltype}': {invalid_str}. Permissible values are: {list(valid_kinds.keys())}"
+        )
         raise ValueError(
             f"Invalid kind value(s) for reltype '{reltype}': {invalid_str}. Permissible values are: {list(valid_kinds.keys())}"
         )
+
+    logging.debug(f"Validation successful for reltype: {reltype}, kinds: {kinds}")
